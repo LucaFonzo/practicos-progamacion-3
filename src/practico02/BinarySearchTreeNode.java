94b22401ee572e0be483146af32ec49b63b520aa
Luca Fonzo <lucafonzo3110@gmail.com>
2023-05-03 19:51:35 -0300

    Hechos algunos metodos

---
+++ src/practico02/BinarySearchTreeNode.java
@@ -0,0 +1,11 @@
+package practico02;
+
+public class BinarySearchTreeNode {
+    private T data;
+    private BinarySearchTree left;
+    private BinarySearchTree right;
+
+    public BinarySearchTreeNode(T data){
+        this.data = data;
+    }
+}
--- src/practico03/GrafoDirigido.java
+++ src/practico03/GrafoDirigido.java
@@ -4,40 +4,73 @@ import java.util.*;
 
 public class GrafoDirigido<T> implements Grafo<T> {
 
+    private Map<Integer,List<Arco>> listaAdyacencia;
+
     public GrafoDirigido()
     {
+        this.listaAdyacencia = new HashMap();
     }
     @Override
-    public void agregarVertice(int verticeId) {
+    public void agregarVertice(int verticeId)
+    {
         // TODO Auto-generated method stub
-        
-    }
-
-    @Override
-    public void borrarVertice(int verticeId) {
-        // TODO Auto-generated method stub
+        this.listaAdyacencia.put(verticeId,new LinkedList<>());
     }
 
     @Override
-    public void agregarArco(int verticeId1, int verticeId2, T etiqueta) {
+    public void borrarVertice(int verticeId)
+    {
         // TODO Auto-generated method stub
+        this.listaAdyacencia.values().stream().forEach(v -> v.remove(verticeId));
+        this.listaAdyacencia.remove(verticeId);
     }
 
     @Override
-    public void borrarArco(int verticeId1, int verticeId2) {
+    public void agregarArco(int verticeId1, int verticeId2, T etiqueta)
+    {
         // TODO Auto-generated method stub
+        if (this.listaAdyacencia.containsKey(verticeId1) && this.listaAdyacencia.containsKey(verticeId2))
+        {
+            Arco<T> arco = new Arco<>(verticeId1,verticeId2,etiqueta);
 
+            if (!this.listaAdyacencia.get(verticeId1).contains(arco))
+            {
+                this.listaAdyacencia.get(verticeId1).add(arco);
+            }
+        }
+    }
+
+    @Override
+    public void borrarArco(int verticeId1, int verticeId2)
+    {
+        // TODO Auto-generated method stub
+        if (this.listaAdyacencia.containsKey(verticeId1) && this.listaAdyacencia.containsKey(verticeId2))
+        {
+            this.listaAdyacencia.get(verticeId1).forEach(a ->
+            {
+                if (a.getVerticeDestino() == verticeId2)
+                {
+                    this.listaAdyacencia.remove(a);
+                    return;
+                }
+            });
+        }
     }
 
     @Override
     public boolean contieneVertice(int verticeId) {
         // TODO Auto-generated method stub
-        return false;
+        return this.listaAdyacencia.containsKey(verticeId);
     }
 
     @Override
     public boolean existeArco(int verticeId1, int verticeId2) {
         // TODO Auto-generated method stub
+        if (this.listaAdyacencia.containsKey(verticeId1) && this.listaAdyacencia.containsKey(verticeId2))
+        {
+            List<Arco> adyacentes = this.listaAdyacencia.get(verticeId1);
+            for ()
+        }
         return false;
     }
 
@@ -81,5 +114,9 @@ public class GrafoDirigido<T> implements Grafo<T> {
     public Iterator<Arco<T>> obtenerArcos(int verticeId) {
         // TODO Auto-generated method stub
         return null;
+    }
+
+    public static void main(String[] args) {
+        GrafoDirigido grafoDirigido = new GrafoDirigido();
     }
 }
